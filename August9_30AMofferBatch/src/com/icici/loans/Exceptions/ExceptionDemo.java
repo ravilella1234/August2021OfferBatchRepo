7ecd0a1f4027db91b63665825df38cca41df7634
ravilella1234 <masters.selenium@gmail.com>
2021-09-17 10:06:29 +0530

    ExceptionHandling

---
+++ August9_30AMofferBatch/src/com/icici/loans/Exceptions/ExceptionDemo.java
@@ -0,0 +1,33 @@
+package com.icici.loans.Exceptions;
+
+import java.util.NoSuchElementException;
+import java.util.Scanner;
+
+public class ExceptionDemo 
+{
+
+	public static void main(String[] args) 
+	{
+		int nr,dr,result;
+		Scanner sc = new Scanner(System.in);
+			
+		while(true) 
+		{
+			System.out.println("Enter the nr value :" );
+			nr = sc.nextInt();
+			System.out.println("Enter the Dr value : ");
+			dr = sc.nextInt();
+			try 
+			{
+				result = nr / dr;
+				System.out.println(result);
+				break;
+			} 
+			catch(Exception e) 
+			{
+				e.printStackTrace();
+			}			
+		}
+	}
+
+}
