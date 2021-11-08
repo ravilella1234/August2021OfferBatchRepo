b217b0d6731cf3a9a9549c154c744e4811deea69
ravilella1234 <masters.selenium@gmail.com>
2021-11-08 10:16:58 +0530

    Actions Class & JavaScript Executors

---
+++ October9AMMavenBatch/src/test/java/actions/ScriptExecutor.java
@@ -0,0 +1,48 @@
+package actions;
+
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class ScriptExecutor 
+{
+
+	public static void main(String[] args) throws Exception 
+	{
+		WebDriver driver;
+		WebDriverManager.chromedriver().setup();
+		driver=new ChromeDriver();
+		driver.get("https://www.facebook.com/");
+		
+		driver.manage().window().maximize();
+		driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+
+		JavascriptExecutor js = (JavascriptExecutor)driver;
+		js.executeScript("document.getElementById('email').value='ravilella'");
+		
+		  String title = js.executeScript("return document.title").toString();
+		  System.out.println(title);
+		  
+		//js.executeScript("window.scrollBy(0,200)");
+		
+		//js.executeScript("window.scrollBy(0,document.body.scrollHeight)");
+		  
+		 js.executeScript("document.getElementById('u_0_2').scrollIntoView()");
+		  
+		  //js.executeScript("window.history.back()");
+		  
+		 // Thread.sleep(3000);
+		  
+		 // js.executeScript("window.history.forward()");
+		  
+		 // Thread.sleep(3000);
+		  
+		 // js.executeScript("window.history.go(0)");
+		
+	}
+
+}
